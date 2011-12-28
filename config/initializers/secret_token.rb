0097543afb57ac215212828773e5b75f3df7fbb0
Vanderlei Facchinetti <vanfloripa@gmail.com>
2011-12-28 20:34:32 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reservation::Application.config.secret_token = '9381880f6aaea0a6a2a70a39736935f435ad955f5eb7b2412c2a4bb50825c1dda1c552414cd07a07c75547dc780d1f5d2c983f575802d58a4050e355c43b05b0'
